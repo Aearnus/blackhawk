c81b52965a7a2ef0246bcb4968970332cd3b7298
Tyler L <tslimkemann42@gmail.com>
2018-06-28 13:25:58 -0700

    added search_yield

--- lib/blackhawk.rb
+++ lib/blackhawk.rb
@@ -69,6 +69,81 @@ module Blackhawk
         # test invocation: 
         # b = Blackhawk::Blackhawk.new 1; b.search_byte_list([1,2,3], debug:true)
         
+        # Search memory using a provided equality block.
+        # @see search_byte_list Example usage
+        #
+        # @param length [Array<Fixnum>] the length of bytes to search process memory for.
+        # @yieldparam byte_list [Array<Fixnum>] the list of bytes to compare
+        # @yieldreturn [Boolean] whether or not that list of bytes produces a match
+        # @param range [Array(Fixnum, Fixnum)] if you want to search through a range of memory. 
+        # @param range [nil] if you want to search the entire process memory.
+        # @param verbose [Boolean] whether or not to print out progress.
+        # @param debug [Boolean] whether or not to print debug information.
+        # @raise [RangeError] if a byte less than 0 or larger than 255 is provided.
+        # @raise [NoMemoryError] if the byte list is longer than a usual map size (1MB).
+        # @return [nil] if no match was found.
+        # @return [Array<MemoryMatch>] if a match was found.
+        def search_yield(length, range: nil, verbose: false, debug: false)
+            verbose = true if debug
+            raise NoMemoryError.new("Can't search for a byte list larger than the 1MB page size") if list.length > (1024 * 1024)
+            raise RangeError.new("Byte out of range") if list.any? { |byte| (byte < 0) || (byte > 255) }
+            
+            map_paths_memo = self.map_paths
+            out = []
+            
+            map_paths_memo.each_with_index do |map_path, map_index|
+                map_range = map_path_to_map_range map_path
+                
+                if verbose
+                    puts "Searching map #{map_range.first.to_s 16} - #{map_range.last.to_s 16} [#{(100 * map_index.to_f / map_paths_memo.length.to_f).round 1}%]..."
+                end
+                
+                map_bytestring = IO.binread(map_path)
+                
+                if map_index == 0
+                    previous_map_file = nil
+                    previous_map_bytestring = "" 
+                else 
+                    previous_map_file = Kernel.open(map_paths_memo[map_index - 1], "rb")
+                    previous_map_file.seek(-list.length, IO::SEEK_END)
+                    previous_map_bytestring = previous_map_file.read
+                    previous_map_file.close
+                end
+                if map_index == map_paths_memo.length - 1
+                    next_map_file = nil
+                    next_map_bytestring = ""
+                else
+                    next_map_file = Kernel.open(map_paths_memo[map_index + 1], "rb")
+                    next_map_bytestring = next_map_file.read list.length
+                    next_map_file.close
+                end
+                
+                # this string is key. this is the string we're going to search for to find the
+                # provided byte list
+                map_bytestring_overhang = 
+                    previous_map_bytestring +
+                    map_bytestring +
+                    next_map_bytestring
+                if debug
+                    puts "    PREVIOUS_MAP: #{previous_map_bytestring.encoding} #{previous_map_bytestring.length}"
+                    puts "    MAP_BYTESTRING: #{map_bytestring_overhang.encoding} #{map_bytestring_overhang.length}"
+                    puts "    NEXT_MAP: #{next_map_bytestring.encoding} #{next_map_bytestring.length}"
+                end
+                
+                # finally, search the memory
+                map_bytestring_overhang.bytes.each_cons(list.length).with_index do |byte_list, overhang_offset|
+                    offset = map_range.first + (overhang_offset - list.length)
+                    if (yield byte_list)
+                        puts "FOUND A MATCH @ #{offset.to_s 16}"
+                        out << MemoryMatch.new([offset, offset + list.length], list)
+                    end
+                end
+            end
+            return out
+        end
+        
+
+        
         # Searches the entire memory for a specified list of bytes.
         #
         # @param list [Array<Fixnum>] the bytes to search the process memory for.
